88a5809c8fcf3c694108db47949c15235b581b25
Kasper Kain <kasperkain@gmail.com>
2023-09-11 01:00:52 -0500

    fixed particle type errors

--- src/components/ParticleBackground/ParticleBackground.tsx
+++ src/components/ParticleBackground/ParticleBackground.tsx
@@ -1,5 +1,5 @@
 import "./ParticleBackground.css";
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const ParticleBackground = () => {
   const canvasRef = useRef(null);
