6becfd22474605271507ab6fd888503322600066
Kasper Kain <kasperkain@gmail.com>
2023-11-15 09:12:20 -0600

    updated toys view

--- src/pages/Toys/Toys.tsx
+++ src/pages/Toys/Toys.tsx
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchData } from "../../services/api";
+import TitleBar from "../../components/TitleBar/TitleBar";
 import TextBox from "../../components/TextBox/TextBox";
+import Button from "../../components/Button/Button";
+import ImageBox from "../../components/ImageBox/ImageBox";
+import { v4 as uuid } from "uuid";
 import PageContainer from "../../components/PageContainer/PageContainer";
 import SectionContainer from "../../components/SectionContainer/SectionContainer";
 import BaseBox from "../../components/BaseBox/BaseBox";
-const Toys: React.FC = () => {
+import ButtonContainer from "../../components/ButtonContainer/ButtonContainer";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
+
+const Games: React.FC = () => {
+  const [toys, setToys] = useState<any[]>();
+
+  useEffect(() => {
+    fetchData("toys").then((data) => {
+      setToys(data);
+    });
+  }, []);
+
   return (
     <PageContainer>
-      <SectionContainer>
-        <BaseBox>
-          <TextBox text='NOTHING HERE YET' />
-        </BaseBox>
-      </SectionContainer>
+      {!toys ? (
+        <LoadingSpinner />
+      ) : (
+        toys.map((toy) => (
+          <SectionContainer
+            key={toy.id || uuid()}
+            titleBar={<TitleBar text={toy.title} />}
+          >
+            <BaseBox>
+              {" "}
+              <ImageBox src={toy.image} />
+              <ButtonContainer>
+                <Button
+                  title='VIEW'
+                  nullMessage={!toy.live ? "unavailable" : undefined}
+                  onClick={() => window.open(toy.live, "_blank")}
+                />
+                <Button
+                  title='SOURCE'
+                  nullMessage={!toy.github ? "unavailable" : undefined}
+                  onClick={() => window.open(toy.github, "_blank")}
+                />
+              </ButtonContainer>
+            </BaseBox>
+          </SectionContainer>
+        ))
+      )}
     </PageContainer>
   );
 };
 
-export default Toys;
+export default Games;
